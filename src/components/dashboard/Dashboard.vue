f0da531cc80a270516419d4b8511acb6f8ee3902
souza-su <rodrigodenner.art@gmail.com>
2023-11-09 20:38:52 -0300

    feat: integraçao db json

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -11,16 +11,22 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>Rodrigo</div>
-        <div>Pão doce</div>
-        <div>Picanha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
-          <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
-          </ul>
+          <div v-if="burger.opcionais && burger.opcionais.length > 0">
+            <ul>
+              <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                {{ opcional }}
+              </li>
+            </ul>
+          </div>
+          <div v-else>
+            <strong>Sem opcionais</strong>
+          </div>
         </div>
         <div>
           <select name="status" class="status">
@@ -37,11 +43,26 @@
 export default {
   name: "Dashboard",
   data() {
-    return {};
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    };
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch("http://localhost:3000/burgers");
+      const data = await req.json();
+      this.burgers = data;
+      console.log(this.burgers);
+    },
+  },
+  mounted() {
+    this.getPedidos();
   },
 };
 </script>
 
 <style scoped>
-  @import "style.css";
+@import "style.css";
 </style>
